f2b8ec5c8b19142569a29b7c2d8dc40f9f98012f
Jeff Fan <tthew851210@gmail.com>
2019-11-28 08:33:21 +0800

    Add US

--- driving.py
+++ driving.py
@@ -6,3 +6,8 @@ if country == '台灣':
         print('你可以開車')
     else: 
         print('你不能開車')
+elif country =='美國':
+    if age >= 16:
+        print('you are allowed to drive')
+    else:
+        print('you are not allowed to drive')
